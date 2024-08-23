5f35ba92ba80b02444e8656802f3a178c5f8779b
Matt Sephton <matt.sephton@gmail.com>
2024-08-23 17:50:18 +0100

    rebuild

--- Extension/SafariWebExtensionHandler.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
-    func beginRequest(with context: NSExtensionContext) {
-        context.completeRequest(returningItems: nil, completionHandler: nil)
-    }
-}
